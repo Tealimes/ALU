9cdcbaed58c2891b5368c99deed9197807332eec
Tealimes <alexpeacock56ten@gmail.com>
2024-12-24 22:15:36 -0500

    Initialized Files

---
+++ ALU.srcs/sources_1/new/ALU.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/24/2024 10:14:38 PM
+// Design Name: 
+// Module Name: ALU
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU(
+
+    );
+endmodule
