bf053e0e6e2fffd06d576b4720365562548af667
Tealimes <alexpeacock56ten@gmail.com>
2024-12-30 11:24:14 -0500

    Added beginning of testbench

--- ALU.srcs/sim_1/new/ALU_tb.sv
+++ ALU.srcs/sim_1/new/ALU_tb.sv
@@ -1,26 +1,33 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/30/2024 11:12:36 AM
-// Design Name: 
-// Module Name: ALU_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
-module ALU_tb(
-
+module ALU_tb();
+    parameter BWIDTH = 32;
+    logic iClk, iRstN;
+    logic signed [BWIDTH-1:0] RS1, RS2;
+    logic [11:0] imm_r;
+    logic [7:0] pc;
+    logic [6:0] opcode;
+    logic [2:0] funct3;
+    logic [6:0] funct7;
+    logic [4:0] shamt;
+    logic [BWIDTH-1:0] RD;
+    
+    ALU#(
+        .BWIDTH(BWIDTH)
+    ) u_ALU (
+        .iClk(iClk), .iRstN(iRstN), .RS1(RS1), .RS2(RS2),
+        .imm_r(imm_r), .pc(pc), .opcode(opcode), .funct3(funct3),
+        .funct7(funct7), .shamt(shamt), .RD(RD)
     );
+    
+    always #5 iClk = ~iClk;
+    
+    initial begin
+        $dumpfile("ALU_tb"); $dumpvars;
+        
+        iClk = 1;
+        
+        $finish;
+    end
 endmodule
--- ALU.srcs/sources_1/new/ALU.v
+++ ALU.srcs/sources_1/new/ALU.v
@@ -4,7 +4,7 @@ module ALU #(
     input iRstN, 
     input signed [BWIDTH-1:0] RS1,
     input signed [BWIDTH-1:0] RS2,
-    input [11:0] imm_r
+    input [11:0] imm_r,
     input [7:0] pc,
     input [6:0] opcode,
     input [2:0] funct3,
@@ -13,7 +13,8 @@ module ALU #(
     output [BWIDTH-1:0] RD
     );
     
-    reg [WIDTH-1:0] RD_t;
+    reg [BWIDTH-1:0] RD_t;
+    
     
     always@(posedge iClk or negedge iRstN) begin
         if(!iRstN) begin
@@ -22,37 +23,39 @@ module ALU #(
         //Register Type
         end else if(opcode == 7'b0110011) begin 
             case(funct3)
-                3'b000 : RD_t <= (Funct7 == 7'b0100000) ? RS1 - RS2 : RS1 + RS2;
+                3'b000 : RD_t <= (funct7 == 7'b0100000) ? RS1 - RS2 : RS1 + RS2;
                 3'b010 : RD_t <= (RS1 < RS2) ? 1'b1 : 1'b0; //signed
                 3'b011 : RD_t <= (RS1 < RS2) ? 1'b1 : 1'b0; //unsigned 
                 3'b100 : RD_t <= RS1 ^ RS2;
                 3'b110 : RD_t <= RS1 | RS2;
                 3'b111 : RD_t <= RS1 & RS2;
                 3'b001 : RD_t <= RS1 << RS2;
-                3'b101 : RD_t <= (Funct7 == 7'b0100000) ? RS1 >>> RS2 : RS1 >> RS2;  
+                3'b101 : RD_t <= (funct7 == 7'b0100000) ? RS1 >>> RS2 : RS1 >> RS2;  
                 default : RD_t <= RD_t;
             endcase
      
         //Immediate Type
         end else if(opcode == 7'b0010011) begin
             case(funct3)
-                3'b000 : RD_t <= (Funct7 == 7'b0100000) ? RS1 - imm_r : RS1 + imm_r;
+                3'b000 : RD_t <= (funct7 == 7'b0100000) ? RS1 - imm_r : RS1 + imm_r;
                 3'b010 : RD_t <= (RS1 < imm_r) ? 1'b1 : 1'b0; //signed
                 3'b011 : RD_t <= (RS1 < imm_r) ? 1'b1 : 1'b0; //unsigned
                 3'b100 : RD_t <= RS1 ^ imm_r;
                 3'b110 : RD_t <= RS1 | imm_r;
                 3'b111 : RD_t <= RS1 & imm_r;
                 3'b001 : RD_t <= RS1 << shamt;
-                3'b101 : RD_t <= (Funct7 == 7'b0100000) ? RS1 >>> shamt : RS1 >> shamt; 
+                3'b101 : RD_t <= (funct7 == 7'b0100000) ? RS1 >>> shamt : RS1 >> shamt; 
                 default RD_t <= RD_t; 
             endcase
             
         //Load Type WIP
         end else if(opcode == 7'b0000011) begin 
-            RD_t <= RD_t
-  
-        end else begin
+            RD_t <= RD_t;
+
+        end else begin 
             RD_t <= 0;
-        end
+        end    
     end
+    
+    assign RD = RD_t;
 endmodule
