99fe5187bb17df311d9b7a8c03f3e1a30de0f2f9
Tealimes <alexpeacock56ten@gmail.com>
2024-12-30 11:13:47 -0500

    Added register and immediate cases

---
+++ ALU.srcs/sim_1/new/ALU_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/30/2024 11:12:36 AM
+// Design Name: 
+// Module Name: ALU_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ALU_tb(
+
+    );
+endmodule
