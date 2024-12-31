265058dfd9c9dd1bf1ee6b64462ca76c4ed0f4d1
Tealimes <alexpeacock56ten@gmail.com>
2024-12-30 21:24:33 -0500

    Added class to help verification

--- ALU.srcs/sim_1/new/ALU_tb.sv
+++ ALU.srcs/sim_1/new/ALU_tb.sv
@@ -1,4 +1,28 @@
-`timescale 1ns / 1ps
+`timescale 1ns / 1ns
+
+class Checker;
+    parameter BWIDTH = 32;
+    logic [BWIDTH-1:0] C_comp;
+    logic [BWIDTH-1:0] C_real;
+    
+    function void equation(input logic A, B, C, opcode, funct7, funct3);
+        C_real = C;
+        if(opcode == 7'b0110011) begin
+            case(funct3)
+               3'b000 : C_comp = A + B;
+               3'b100 : C_comp <= A ^ B;
+            endcase
+        end
+    endfunction
+    function void display();
+        if(C_comp == C_real) begin
+            $display("Valid Output!");
+        end else begin
+            $display("C should be 0x%0h, but is instead 0x%0h", C_comp, C_real);
+        end
+        
+    endfunction
+endclass
 
 
 module ALU_tb();
@@ -7,11 +31,15 @@ module ALU_tb();
     logic signed [BWIDTH-1:0] RS1, RS2;
     logic [11:0] imm_r;
     logic [7:0] pc;
+    logic [BWIDTH-1:0] RS1;
+    logic [BWIDTH-1:0] RS2;
     logic [6:0] opcode;
     logic [2:0] funct3;
     logic [6:0] funct7;
     logic [4:0] shamt;
     logic [BWIDTH-1:0] RD;
+    
+    Checker check;
     
     ALU#(
         .BWIDTH(BWIDTH)
@@ -25,8 +53,36 @@ module ALU_tb();
     
     initial begin
         $dumpfile("ALU_tb"); $dumpvars;
-        
+        check = new();
         iClk = 1;
+        iRstN = 0;
+        
+        #10 iRstN = 1;
+        
+        //XOR Test
+        opcode = 7'b0110011;
+        funct7 = 0;
+        funct3 = 3'b100;
+        RS1 = $random;
+        RS2 = $random;
+        
+        check.equation(RS1,RS2,RD,opcode,funct7,funct3);
+        check.display();
+       
+        #10;
+        
+        //ADD test
+        opcode = 7'b0110011;
+        funct7 = 0;
+        funct3 = 3'b000;
+        RS1 = $random;
+        RS2 = $random;
+        
+        check.equation(RS1,RS2,RD,opcode,funct7,funct3);
+        check.display();
+        #100;
+        
+        
         
         $finish;
     end
